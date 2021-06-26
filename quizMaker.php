7610213e9bf691bf0204bef904d987ba90bb2b33
Fitrafep <fitra.fep@gmail.com>
2021-06-26 18:17:58 +0700

    change name to username

--- quizMaker.php
+++ quizMaker.php
@@ -26,7 +26,7 @@
                 <form action="../Kokaku/dashboard/index.php" method="get">
                     <div class="row px-3"> <label class="mb-1">
                             <h6 class="mb-0 text-sm">Username</h6>
-                        </label> <input class="mb-4" type="text" name="name" placeholder="Enter an username" required> </div>
+                        </label> <input class="mb-4" type="text" name="username" placeholder="Enter an username" required> </div>
                     <div class="row px-3"> <label class="mb-1">
                             <h6 class="mb-0 text-sm">Password</h6>
                         </label> <input type="password" name="password" placeholder="Enter password" required> </div>
